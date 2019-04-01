ccdd3eef1c496983798457bcf30122c3dfb75fef
ecas0004 <akram.munshi@exzatechconsulting.com>
2019-04-01 18:18:03 +0530

    test

---
+++ demo/src/main/java/com/example/demo/Car.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import lombok.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Car {
+    @Id @GeneratedValue
+    private long id;
+    private @NonNull String name;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
